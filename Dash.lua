2a9d18e297bcf70dd6552adc3228116841ed1992
Blacklq3 <deldemce@gmail.com>
2020-07-28 15:29:13 +0200

    28-07-2020 3:29PM

--- Dash.lua
+++ Dash.lua
@@ -2,87 +2,87 @@
       local character = player.Character
       local hum = character:WaitForChild("Humanoid")
       local rootpart = character:WaitForChild("HumanoidRootPart")	
-	  local tapped = false
-	  local time1 = 0.25	
-		
-	   function Animation(Anim)
-	     local track = hum:LoadAnimation(Anim)
-		 track.Priority = Enum.AnimationPriority.Movement
-		 track:Play()	
-	   end
-		
-	  local keys = {}
+      local tapped = false
+      local time1 = 0.25	
+        
+       function Animation(Anim)
+         local track = hum:LoadAnimation(Anim)
+         track.Priority = Enum.AnimationPriority.Movement
+         track:Play()	
+       end
+        
+      local keys = {}
       keys[1] = "W"  
       keys[2] = "A"
       keys[3] = "S"
       keys[4] = "D"
 
-	  local UIS = game:GetService("UserInputService")
-	  local debounce = true	
-		  
-		
-		function Dashed()
-			local bool = Instance.new('BoolValue',workspace)
-			bool.Name = "DashedBool"
-			bool.Value = true
-			debris:AddItem(bool, 0.57)
-		end
-		
-		function DoneDashed()
-			local bool = workspace:WaitForChild("DashedBool")
-			bool.Value = false
-		end
-		
-		
-	  function BodyVelocity(Multiplier,Rotation)	
-		local bodyvelocity1 = Instance.new('BodyVelocity',rootpart)
-		bodyvelocity1.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
-		local LCframe = player.Character.HumanoidRootPart.CFrame * CFrame.fromEulerAnglesXYZ(math.rad(0),math.rad(Rotation),math.rad(0))
-		bodyvelocity1.Velocity = LCframe.lookVector*Multiplier	
-		debris:AddItem(bodyvelocity1, 0.5)	 		
-	  end
-		
-		
-	  function DoDash(Key,Rotation,UIS,debounce,tapped,time1,CD)
-	   UIS.InputBegan:Connect(function(input,IsTyping)
-	    if IsTyping then return end
-		 if input.KeyCode == Enum.KeyCode[Key] and debounce then	
-		  debounce = false
-		   if hum:GetState() == Enum.HumanoidStateType.Jumping then return end
-		    if hum:GetState() == Enum.HumanoidStateType.Freefall then
-			 if not tapped then 
-			   tapped = true
-			   wait(time1)	
-			   tapped = false
-			   else		
-			    Dashed()
-			    BodyVelocity(40,Rotation)
-			    wait(0.5)
-			    DoneDashed()
-			   end	
-			    else
-			     if not tapped then 
-			      tapped = true	
-			      wait(time1)
-			      tapped = false
-			      else		
-			       Dashed()
-			       BodyVelocity(55,Rotation)
-				   wait(0.5)
-				   DoneDashed()
-				  end
-	            end
-			   end
-			  debounce = true
+      local UIS = game:GetService("UserInputService")
+      local debounce = true	
+          
+        
+        function Dashed()
+            local bool = Instance.new('BoolValue',workspace)
+            bool.Name = "DashedBool"
+            bool.Value = true
+            debris:AddItem(bool, 0.57)
+        end
+        
+        function DoneDashed()
+            local bool = workspace:WaitForChild("DashedBool")
+            bool.Value = false
+        end
+        
+        
+      function BodyVelocity(Multiplier,Rotation)	
+        local bodyvelocity1 = Instance.new('BodyVelocity',rootpart)
+        bodyvelocity1.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+        local LCframe = player.Character.HumanoidRootPart.CFrame * CFrame.fromEulerAnglesXYZ(math.rad(0),math.rad(Rotation),math.rad(0))
+        bodyvelocity1.Velocity = LCframe.lookVector*Multiplier	
+        debris:AddItem(bodyvelocity1, 0.5)	 		
+      end
+        
+        
+      function DoDash(Key,Rotation,UIS,debounce,tapped,time1,CD)
+       UIS.InputBegan:Connect(function(input,IsTyping)
+        if IsTyping then return end
+         if input.KeyCode == Enum.KeyCode[Key] and debounce then	
+          debounce = false
+           if hum:GetState() == Enum.HumanoidStateType.Jumping then return end
+            if hum:GetState() == Enum.HumanoidStateType.Freefall then
+             if not tapped then 
+               tapped = true
+               wait(time1)	
+               tapped = false
+               else		
+                Dashed()
+                BodyVelocity(40,Rotation)
+                wait(0.5)
+                DoneDashed()
+               end	
+                else
+                 if not tapped then 
+                  tapped = true	
+                  wait(time1)
+                  tapped = false
+                  else		
+                   Dashed()
+                   BodyVelocity(55,Rotation)
+                   wait(0.5)
+                   DoneDashed()
+                  end
+                end
+               end
+              debounce = true
              end)
-	       end		
+           end		
             
          local Count = 0    
         
          
 
 
-
+         -- Calling out 
          for i = 1,4 do 
             Count+= 90 
              if Count ~= 270
@@ -92,5 +92,5 @@
              end   
          end   
          
-	
-	   	
+    
+           
